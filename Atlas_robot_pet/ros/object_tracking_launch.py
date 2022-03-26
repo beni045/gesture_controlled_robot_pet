0ce1e78d08e4985cad4c8ad1624b34a93c154bb0
PN018 <ubc.ca>
2022-03-26 05:18:54 +0000

    Ros: Fixed take a picture mode.

---
+++ Atlas_robot_pet/ros/object_tracking_launch.py
@@ -0,0 +1,133 @@
+#!/usr/bin/env python3
+import random
+import rospy
+from std_msgs.msg import String
+from sensor_msgs.msg import Image
+import os
+import cv2
+import numpy as np
+import argparse
+import sys
+sys.path.append('../projects/demo/Atlas_robot_pet')
+from model_processor import handpose_ModelProcessor
+from model_processor import face_detection_ModelProcessor
+from model_processor import hand_detection_ModelProcessor
+from model_processor import body_pose_ModelProcessor
+from model_processor import object_tracking_ModelProcessor
+from atlas_utils.camera import Camera
+from atlas_utils import presenteragent
+from atlas_utils.acl_image import AclImage
+import acl
+from queue import Queue
+from acl_resource import AclResource
+from cv_bridge import CvBridge, CvBridgeError
+from rospy.exceptions import ROSException, ROSSerializationException, ROSInitException, ROSInterruptException
+import socket
+#from Serial_servo_v1 import *
+import threading
+import time
+import io
+import struct
+import pickle
+import zlib
+
+MODEL_PATH_OBJECT_TRACKING = "../hand_gesture_controlled_robot_pet/models/mot_v2.om"
+BODYPOSE_CONF="../hand_gesture_controlled_robot_pet/Atlas_robot_pet/param.conf"
+
+class object_tracking_launch():
+    def __init__(self):
+    	# initiliaze
+        rospy.init_node('object_tracking_launch', anonymous=True)
+        self.pub = rospy.Publisher('object_tracking', String, queue_size=10)
+        self.objimgpub = rospy.Publisher('objcamera', Image, queue_size=10)
+        self.image_queue = Queue(maxsize=1)
+        self.acl_resource = AclResource()
+        self.acl_resource.init()
+        object_tracking_model_parameters = {
+            'model_dir': MODEL_PATH_OBJECT_TRACKING,
+        }
+        self.object_tracking_model_processor = object_tracking_ModelProcessor(self.acl_resource,object_tracking_model_parameters)
+        rospy.loginfo("peeko")
+        self.Subscriber()
+    def Subscriber(self):
+        rospy.Subscriber("camera", Image, self.process_image, queue_size = 10, buff_size = 2 ** 24)
+        #rospy.spin()
+# publisher for img from object to robot pet launch        
+    def convert_and_pubish(self, image_data):
+        rospy.loginfo(image_data.shape)
+        try:
+            img_msg = CvBridge().cv2_to_imgmsg(image_data, "rgb8")
+            rospy.loginfo(type(img_msg))
+
+            self.objimgpub.publish(img_msg)
+        except CvBridgeError as err:
+            rospy.logerr("Ran into exception when converting message type with CvBridge. See error below:")
+            raise err
+            
+        except ROSSerializationException as err:
+            rospy.logerr("Ran into exception when serializing message for publish. See error below:")
+            raise err
+        except ROSException as err:
+            raise err
+        except ROSInterruptException as err:
+            rospy.loginfo("ROS Interrupt.")
+            raise err
+
+    def object_tracking(self):
+        blank_image = np.zeros((1,1,3), np.uint8)
+        curr_tid = -1
+        count_notrack = 0
+        global RGBMatrix
+        RGBMatrix = blank_image
+        while not rospy.is_shutdown():
+            object_tracking_flag = rospy.get_param('/object_tracking_flag')
+            if object_tracking_flag:
+                coords = rospy.get_param('/coords')
+                rospy.set_param('/rpi_signal_flag', 1)
+                try:
+                    if not self.image_queue.empty():
+                        img_original = self.image_queue.get()
+                        canvas, command, hg_command, next_tid = self.object_tracking_model_processor.predict(img_original, coords, curr_tid)
+                        if next_tid == -1:
+                            count_notrack += 1
+                            if count_notrack == 50:
+                                count_notrack = 0
+                                curr_tid = -1
+                                rospy.set_param('/object_tracking_flag', 0)
+                                rospy.set_param('/rpi_signal_flag', 0)
+                        else:
+                            curr_tid = next_tid
+                            count_notrack = 0
+
+                        #canvas = cv2.cvtColor(canvas, cv2.COLOR_BGR2RGB, 3)
+                        self.convert_and_pubish(canvas)
+
+                        rospy.loginfo(command)
+                        self.pub.publish(command)
+                        self.pub.publish(hg_command)
+
+                        _,jpeg_image = cv2.imencode('.jpg',canvas)
+                        jpeg_image = AclImage(jpeg_image, img_original.shape[0], img_original.shape[1], jpeg_image.size)
+                        rospy.loginfo("----Object Tracked----")
+                except(ROSException, CvBridgeError, ROSSerializationException) as err:
+                    rospy.logerr("Ran into exception when converting message type with CvBridge. See error below:")
+                    raise err
+            else:
+                curr_tid = -1
+                count_notrack = 0
+    
+    def process_image(self, data):
+        if not self.image_queue.full():
+            try:
+                rgb_img = CvBridge().imgmsg_to_cv2(data, "rgb8")
+                self.image_queue.put(rgb_img)
+            except CvBridgeError as err:
+                raise err
+
+
+if __name__ == '__main__':
+    try:
+        object_tracking_launch_node = object_tracking_launch()
+        object_tracking_launch_node.object_tracking()
+    except rospy.ROSInterruptException:
+        pass
---
+++ Atlas_robot_pet/ros/rpi_signal_launch.py
@@ -0,0 +1,318 @@
+#!/usr/bin/env python3
+import random
+import rospy
+from std_msgs.msg import String
+from sensor_msgs.msg import Image
+import os
+import cv2
+import numpy as np
+import argparse
+import sys
+from queue import Queue
+from std_msgs.msg import UInt8
+from rospy.numpy_msg import numpy_msg
+from cv_bridge import CvBridge, CvBridgeError
+from rospy.exceptions import ROSException, ROSSerializationException, ROSInitException, ROSInterruptException
+
+sys.path.append('../hand_gesture_controlled_robot_pet/Atlas_robot_pet')
+# from model_processor import handpose_ModelProcessor
+# from model_processor import face_detection_ModelProcessor
+# from model_processor import hand_detection_ModelProcessor
+# from model_processor import body_pose_ModelProcessor
+# from atlas_utils.camera import Camera
+# from atlas_utils import presenteragent
+# from atlas_utils.acl_image import AclImage
+# import acl
+# from acl_resource import AclResource
+import socket
+#from Serial_servo_v1 import *
+import threading
+import time
+import io
+import struct
+import pickle
+import zlib
+
+MODEL_PATH_BODY_POSE = "../hand_gesture_controlled_robot_pet/Atlas_robot_pet/model/body_pose.om"
+BODYPOSE_CONF="../hand_gesture_controlled_robot_pet/Atlas_robot_pet/param.conf"
+
+class rpi_signal_launch():
+    def __init__(self):
+    	# initiliaze
+        rospy.init_node('rpi_signal_launch', anonymous=True)
+        self.UDP_IP = '192.168.8.117' # Set to destination IP, RPi in this case
+        self.UDP_PORT = 12345      # Port #, make sure same as on RPi
+        
+        # If not connected to Raspberry Pi first time, keep trying to connect
+        connection_established = 0
+        while connection_established == 0:
+            try:
+                rospy.loginfo("socket.create")
+                self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                rospy.loginfo("sock.connect")
+                self.sock.connect((self.UDP_IP, self.UDP_PORT))
+                connection_established = 1
+                rospy.loginfo("CONNECTED")
+            except:
+                connection_established = 0
+                rospy.loginfo("trying to connect...")
+        
+        rospy.loginfo("!!!!!!!!!!!!peeko YAY !!!!!!!!!!!!!")
+        blank_image = np.zeros((1,1,3), np.uint8)
+        self.RGBMatrix = blank_image
+        self.face_centered = 0
+        self.image_queue = Queue(maxsize=1)
+        self.data = b""
+        self.hg_sig = "none"
+        self.received_sig = "none"
+        self.face_sig = "nothing"
+        self.payload_size = struct.calcsize(">L")
+        self.Subscriber()
+        self.face_Sub()
+        self.rgbSubscriber()
+        self.follow_Sub()
+    def Subscriber(self):
+        rospy.Subscriber("pet_gestures", String, self.hg_signal)    
+    def hg_signal(self, sig_data):
+        if sig_data.data == "STOP FOLLOW" or not rospy.get_param('/object_tracking_flag'):
+            self.hg_sig = sig_data.data
+    def face_Sub(self): 
+        rospy.Subscriber("face", String, self.face_signal)
+    def face_signal(self, sig_data):
+        self.face_sig = sig_data.data
+    def follow_Sub(self):
+        rospy.Subscriber("object_tracking", String, self.follow_signal)
+    def follow_signal(self, sig_data):
+        if sig_data.data in ['UP', 'DOWN', 'LEFT', 'RIGHT', 'CENTERED']:
+            self.face_sig = sig_data.data
+        else:
+            self.hg_sig = sig_data.data
+            self.face_sig = "NOTHING"
+    def rgbSubscriber(self):
+        rospy.Subscriber("RGBmatrix", Image, self.rgb_signal)
+    def rgb_signal(self, sig_data):
+        if not self.image_queue.full():
+            try:
+                rgb_img = CvBridge().imgmsg_to_cv2(sig_data, "rgb8")
+                self.image_queue.put(rgb_img)
+            except CvBridgeError as err:
+                raise err
+
+    def rpi_signal(self):
+        while not rospy.is_shutdown():
+            rospy.loginfo("waiting for flag")
+            rpi_signal_flag = rospy.get_param("/rpi_signal_flag")
+            if rpi_signal_flag:
+                face_detection_flag = rospy.get_param("/face_detection_flag")
+                object_tracking_flag = rospy.get_param('/object_tracking_flag')
+                rospy.loginfo("flag received")
+                self.data = b""
+                self.payload_size = struct.calcsize(">L")
+                if not self.image_queue.empty():
+                    self.RGBMatrix = self.image_queue.get()
+                else:
+                    self.RGBMatrix = np.zeros((1,1,3), np.uint8)
+
+                if self.face_centered and self.received_sig  == "take_a_picture":
+                        encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
+                        rospy.loginfo("-----------PEEEEEEEEEEEEEEEEEEEEEKKKKKKKKKKKKKKKKKKKKKKKKKOOOOOOOOOOOOOOOOOO------------------------")
+                        result, send_frame = cv2.imencode('.jpg', self.RGBMatrix, encode_param)
+                        self.data = pickle.dumps(send_frame, 0)
+
+                elif self.face_sig != "NOTHING": #and self.hg_sig  == "STOP":
+
+                    if self.face_sig =='UP':
+                        rospy.loginfo("Sending: Servo Up")
+                        self.data = pickle.dumps("servo up\n", 0)
+                    
+                    elif self.face_sig =='DOWN':
+                        rospy.loginfo("Sending: Servo Down")
+                        self.data = pickle.dumps("servo down\n", 0)
+                    
+                    elif self.face_sig =='LEFT':
+                        rospy.loginfo("Sending: Servo Left")
+                        self.data = pickle.dumps("servo left\n", 0)
+                    
+                    elif self.face_sig =='RIGHT':
+                        rospy.loginfo("Sending: Servo Right")
+                        self.data = pickle.dumps("servo right\n", 0)
+                    
+                    elif self.face_sig == 'CENTERED':
+                        rospy.loginfo("Face centered")
+                        self.face_centered = 1
+
+                    self.face_sig = "NOTHING"
+
+                #####################################################
+                ### Sending hand gesture commands to Raspberry Pi ###
+                #####################################################
+
+                elif self.hg_sig  == "ACTIVATE" and self.received_sig != "take_a_picture" and not object_tracking_flag:
+                    rospy.loginfo("Sending: Activate")
+                    self.data = pickle.dumps("activate\n", 0)
+
+                elif self.hg_sig  == "DEACTIVATE" and self.received_sig != "take_a_picture" and not object_tracking_flag:
+                    rospy.loginfo("Sending: Deactivate")
+                    self.data = pickle.dumps("deactivate\n", 0)
+
+                elif self.hg_sig  == "TAKE A PICTURE" and self.received_sig != "take_a_picture" and not object_tracking_flag:
+                    rospy.loginfo("Sending: Take a Picture")
+                    self.data = pickle.dumps("take a picture\n", 0)
+
+                elif self.hg_sig  == "SPIN" and self.received_sig != "take_a_picture" and not object_tracking_flag:
+                    rospy.loginfo("Sending: Spin")
+                    self.data = pickle.dumps("spin\n", 0)
+
+                elif self.hg_sig  == "FORWARDS" and self.received_sig != "take_a_picture":
+                    rospy.loginfo("Sending: Forward")
+                    self.data = pickle.dumps("forward\n", 0)
+
+                elif self.hg_sig  == "BACKWARDS" and self.received_sig != "take_a_picture":
+                    rospy.loginfo("Sending: Backward")
+                    self.data = pickle.dumps("backward\n", 0)
+
+                elif self.hg_sig  == "LEFT" and self.received_sig != "take_a_picture":
+                    rospy.loginfo("Sending: Left")
+                    self.data = pickle.dumps("left\n", 0)
+
+                elif self.hg_sig  == "RIGHT" and self.received_sig != "take_a_picture":
+                    rospy.loginfo("Sending: Right")
+                    self.data = pickle.dumps("right\n", 0)
+
+                elif self.hg_sig  == "FOLLOW" and self.received_sig != "take_a_picture":
+                    rospy.loginfo("Sending: Follow")
+                    self.data = pickle.dumps("follow\n", 0)
+                    
+                elif self.hg_sig  == "STOP FOLLOW" and self.received_sig != "take_a_picture":
+                    rospy.loginfo("Sending: Stop Follow")
+                    self.data = pickle.dumps("stop_follow\n", 0)
+                    
+                # elif self.hg_sig  == "BODY" :
+                #     rospy.loginfo("Sending: Body")
+                #     self.data = pickle.dumps("body\n", 0)
+
+                if self.data == b"":
+                    #    rospy.loginfo("Sending: None")
+                    self.data = pickle.dumps("none\n", 0)
+
+                ##################################################### 
+
+                
+                rospy.loginfo(self.hg_sig)
+                rospy.loginfo(self.face_sig)
+                # Send to Raspberry Pi
+                size = len(self.data)
+                self.sock.sendall(struct.pack(">L", size) + self.data)
+
+                # Reset data for receiving response from Raspberry Pi
+                self.data = b""
+
+                #################################################
+                ### Receive acknowledgement from Raspberry Pi ###
+                #################################################
+
+                while len(self.data) < self.payload_size:
+                    #  rospy.loginfo("Recv: {}".format(len(data)))
+                    self.data += self.sock.recv(4096)
+
+                #   rospy.loginfo("Done Recv: {}".format(len(data)))
+                packed_msg_size = self.data[:self.payload_size]
+                self.data = self.data[self.payload_size:]
+                msg_size = struct.unpack(">L", packed_msg_size)[0]
+                #   rospy.loginfo("msg_size: {}".format(msg_size))
+
+                while len(self.data) < msg_size:
+                    self.data += self.sock.recv(4096)
+
+                frame_data = self.data[:msg_size]
+                self.data = self.data[msg_size:]
+                self.data=pickle.loads(frame_data, fix_imports=True, encoding="bytes")
+
+                if str(self.data) != "got it \n":
+                    rospy.loginfo("Received data -----------------------------------")
+                    rospy.loginfo(self.data)
+
+                ######################################################
+                ### Check if Raspberry Pi received serial commands ###
+                ######################################################
+
+                if self.data == "received activate\n":
+                    self.received_sig  = "activate"
+                    rospy.loginfo(self.received_sig )
+
+                if self.data == "received deactivate\n":
+                    self.received_sig  = "deactivate"
+                    rospy.loginfo(self.received_sig )
+
+                if self.data == "received take a picture\n":
+                    self.received_sig  = "take_a_picture"
+                    rospy.loginfo(self.received_sig )
+                    if self.face_centered:
+                        rospy.set_param("/body_face_detection_flag", 0)
+
+                if self.data == "received spin\n":
+                    self.received_sig  = "spin"
+                    rospy.loginfo(self.received_sig )
+
+                if self.data == "received forward\n":
+                    self.received_sig  = "forward"
+                    rospy.loginfo(self.received_sig )
+
+                if self.data == "received backward\n":
+                    self.received_sig  = "backward"
+                    rospy.loginfo(self.received_sig )
+
+                if self.data == "received left\n":
+                    self.received_sig  = "left"
+                    rospy.loginfo(self.received_sig )
+
+                if self.data == "received right\n":
+                    self.received_sig  = "right"
+                    rospy.loginfo(self.received_sig )
+                
+                if self.data == "received follow\n":
+                    self.received_sig  = "follow"
+                    rospy.loginfo(self.received_sig )
+                
+                if self.data == "received stop_follow\n":
+                    self.received_sig  = "stop_follow"
+                    rospy.set_param("/object_tracking_flag", 0)
+                    rospy.loginfo(self.received_sig )
+
+                if self.data == "done command\n":
+                    rospy.set_param("/face_detection_flag", 0)
+                    rospy.set_param("/rpi_signal_flag", 0)
+                    self.face_centered = 0
+                    self.received_sig  = "STOP"
+                    rospy.loginfo(self.received_sig )
+            else:
+                self.data = pickle.dumps("none\n", 0)
+                size = len(self.data)
+                self.sock.sendall(struct.pack(">L", size) + self.data)
+
+                # Reset data for receiving response from Raspberry Pi
+                self.data = b""
+                while len(self.data) < self.payload_size:
+                    #  rospy.loginfo("Recv: {}".format(len(data)))
+                    self.data += self.sock.recv(4096)
+
+                #   rospy.loginfo("Done Recv: {}".format(len(data)))
+                packed_msg_size = self.data[:self.payload_size]
+                self.data = self.data[self.payload_size:]
+                msg_size = struct.unpack(">L", packed_msg_size)[0]
+                #   rospy.loginfo("msg_size: {}".format(msg_size))
+
+                while len(self.data) < msg_size:
+                    self.data += self.sock.recv(4096)
+
+                frame_data = self.data[:msg_size]
+                self.data = self.data[msg_size:]
+                self.data=pickle.loads(frame_data, fix_imports=True, encoding="bytes")
+                self.data = b""
+
+if __name__ == '__main__':
+    try:
+        launch = rpi_signal_launch()
+        launch.rpi_signal()
+    except rospy.ROSInterruptException:
+        pass
